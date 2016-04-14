9c5301eac0a1fd1a50e966b026050b60103f50f3
Bob Soppe <github@bobsoppe.nl>
2016-04-14 15:39:57 +0200

    Updated forms creation to include travel product forms

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,11 +1,11 @@
 package Main;
 
-import Views.TravelLocationOverviewFrame;
-import Views.TravelMethodOverviewFrame;
+import Views.*;
 
 public class Main {
 	public static void main(String[] args) {
 		new TravelMethodOverviewFrame();
 		new TravelLocationOverviewFrame();
+		new TravelProductOverviewFrame();
 	}
 }
